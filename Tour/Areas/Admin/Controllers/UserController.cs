b8151725f2e777def221447e6f2bd996d5d5529d
gunayshkh <gunayshkh@outlook.com>
2022-04-28 12:06:20 +0400

    Revert "Revert "admin2""
    This reverts commit 275bd7f413e646428867f303b99800fee7b3a521.

---
+++ Tour/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Voyage.DAL;
+
+namespace Voyage.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController: Controller
+    {
+        private readonly VoyageDbContext _db;
+
+        public UserController(VoyageDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
